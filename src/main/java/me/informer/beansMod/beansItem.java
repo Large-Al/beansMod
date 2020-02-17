c58b33de4838593194ae614788823a990ce71af7
Nathaniel Noble <nnoble@liberty.edu>
2020-02-17 18:20:34 -0500

    Initial Commit

---
+++ src/main/java/me/informer/beansMod/beansItem.java
@@ -0,0 +1,27 @@
+package me.informer.beansMod;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+
+public class beansItem extends Item {
+    public beansItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
+        if (playerEntity.getMainHandStack().getItem() == beansMod.BEANS_CLOSED_ITEM) {
+            //When right clicked, the closed can of beans (BEANS_CLOSED_ITEM) will be replaced with the open can of beans (BEANS_ITEM)
+
+            playerEntity.inventory.getMainHandStack().decrement(1);
+            playerEntity.inventory.insertStack(new ItemStack(beansMod.BEANS_ITEM));
+        }
+        return new TypedActionResult<>(ActionResult.SUCCESS, playerEntity.getStackInHand(hand));
+    }
+}
