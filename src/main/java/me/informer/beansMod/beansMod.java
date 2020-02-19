cb73d6b361086d463ac0a9dec5090fcc031b1f4b
Nathaniel Noble <nnoble@liberty.edu>
2020-02-19 16:51:40 -0500

    Added coffee and its effect, still need to fix the icon for the potion

--- src/main/java/me/informer/beansMod/beansMod.java
+++ src/main/java/me/informer/beansMod/beansMod.java
@@ -2,8 +2,13 @@ package me.informer.beansMod;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
-import net.minecraft.block.Blocks;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffectType;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.*;
+import net.minecraft.potion.Potion;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -18,8 +23,10 @@ public class beansMod implements ModInitializer {
     public static final beansItem BEANS_CLOSED_ITEM = new beansItem(new Item.Settings().group(ITEM_GROUP));
     public static final beansItem BEANS_EMPTY_ITEM = new beansItem(new Item.Settings().group(ITEM_GROUP));
 
-    //public static final Item COFFEE_ITEM = new Item(new Item.Settings().group(ItemGroup.FOOD).group(ITEM_GROUP))
+    public static final beansStatusEffect COFFEE_EFFECT = new beansStatusEffect(StatusEffectType.BENEFICIAL, 1427050);
 
+    //Texture will go in texture/mob_effect
+    public static final Potion COFFEE = new Potion(new StatusEffectInstance[]{new StatusEffectInstance(COFFEE_EFFECT, 3600)});
 
     @Override
     public void onInitialize() {
@@ -31,7 +38,8 @@ public class beansMod implements ModInitializer {
 
         //COFFEE
 
-
+        Registry.register(Registry.POTION, "coffee", COFFEE);
+        Registry.register(Registry.STATUS_EFFECT, 33, "coffee_effect", COFFEE_EFFECT).addAttributeModifier(EntityAttributes.MOVEMENT_SPEED, "91AEAA56-376B-4498-935B-2F7F68070635", 0.20000000298023224D,EntityAttributeModifier.Operation.MULTIPLY_TOTAL).addAttributeModifier(EntityAttributes.ATTACK_SPEED, "AF8B6E3F-3328-4C0A-AA36-5BA2BB9DBEF3", 0.10000000149011612D, EntityAttributeModifier.Operation.MULTIPLY_TOTAL);
 
     }
 }
---
+++ src/main/java/me/informer/beansMod/beansStatusEffect.java
@@ -0,0 +1,10 @@
+package me.informer.beansMod;
+
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectType;
+
+public class beansStatusEffect extends StatusEffect {
+    public beansStatusEffect(StatusEffectType type, int color) {
+        super(type, color);
+    }
+}
